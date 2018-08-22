09ecdba04a750f73ffd3959431c9328183019e46
makingthematrix <makingthematrix@gmail.com>
2018-08-22 19:43:43 +0200

    console parameters

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,19 +4,24 @@ import langtonscell.LangtonsCell
 import visualisation.BoardWindow
 
 object Main {
-  val dim = 200
-
-  private def init(board: Board[LangtonsCell]) =
-    board.update(Pos2D(dim / 2, dim / 2)) {
-      _.copy(dir = Some(Up))
-    }
 
   def main(args: Array[String]): Unit = {
+    var dim = 200
+    var it = 100
+    args.sliding(2, 2).foreach {
+      case Array("dim", d) => println(s"dim = $d"); dim = Integer.parseInt(d)
+      case Array("it", i)  => println(s"it  = $i"); it = Integer.parseInt(i)
+      case x => println(s"x = ${x.toList}");
+    }
+
+    val init = (board: Board[LangtonsCell]) => board.update(Pos2D(dim / 2, dim / 2)) { _.copy(dir = Some(Up)) }
+
     val auto = new Automaton(dim, init, LangtonsCell.apply)
     val world = BoardWindow("Langtons Ant", dim = dim, scale = 4)
 
     val timestamp = System.currentTimeMillis()
-    for (_ <- 1 to 100) world.draw(auto.iterator.next())
+    for (_ <- 1 to it) world.draw(auto.iterator.next())
     println(s"${System.currentTimeMillis() - timestamp}")
   }
+
 }
