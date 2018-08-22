44f0c9da00c7178c2e657150f1a62cc37cb1d434
makingthematrix <makingthematrix@gmail.com>
2018-08-22 23:12:34 +0200

    step over iterations

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -8,10 +8,12 @@ object Main {
   def main(args: Array[String]): Unit = {
     var dim = 200
     var it = 100
+    var step = 1
     args.sliding(2, 2).foreach {
-      case Array("dim", d) => println(s"dim = $d"); dim = Integer.parseInt(d)
-      case Array("it", i)  => println(s"it  = $i"); it = Integer.parseInt(i)
-      case x => println(s"x = ${x.toList}");
+      case Array("dim", d)  => println(s"dim  = $d"); dim  = Integer.parseInt(d)
+      case Array("it", i)   => println(s"it   = $i"); it   = Integer.parseInt(i)
+      case Array("step", s) => println(s"step = $s"); step = Integer.parseInt(s)
+      case x => println(s"unrecognized parameters: ${x.toList}");
     }
 
     val init = (board: Board[LangtonsCell]) => board.copy(Pos2D(dim / 2, dim / 2)) { _.copy(dir = Some(Up)) }
@@ -20,7 +22,12 @@ object Main {
     val world = BoardWindow("Langtons Ant", dim = dim, scale = 4)
 
     val timestamp = System.currentTimeMillis()
-    for (_ <- 1 to it) world.draw(auto.iterator.next())
+    for (i <- 0 until it){
+      val board = auto.iterator.next()
+      if (i % step == 0) world.draw(board)
+    }
+    world.draw(auto.iterator.next())
+
     println(s"${System.currentTimeMillis() - timestamp}")
   }
 
